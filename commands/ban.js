94a3e83a8fdbea39a0c33ecd3cb9d7d3164cde69
greysdawn <greysdawn@gmail.com>
2020-01-04 18:31:00 -0500

    Oops

--- commands/ban.js
+++ commands/ban.js
@@ -224,7 +224,7 @@ module.exports.subcommands.edit = {
 				fields: [
 				{
 					name: "**__Last Known Usernames__**",
-					value: users.map(m => `${m.info.username}#${m.info.discriminator}`).join("\n")) || "Something went wrong"
+					value: users.map(m => `${m.info.username}#${m.info.discriminator}`).join("\n") || "Something went wrong"
 				},
 				{
 					name: "**__User IDs__**",
